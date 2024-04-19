8289236ee8f5700e009eb7fb2f96d4ed0570e180
Benjamin Preston <bpre001@aucklanduni.ac.nz>
2024-04-19 13:47:45 +1200

    cleaned up date conventions

--- wrangle.R
+++ wrangle.R
@@ -1,4 +1,4 @@
-## Create cohause wide tibble 
+## Create cohaus wide tibble 
 ## 
 
 library(tidyverse)
@@ -8,16 +8,18 @@ library(suncalc)
 
 # read in cohaus use and generation data
 cohaus <- read_csv("data/cohaus.csv") |> 
-  mutate(dttm = parse_date_time(dttm, "dmy_HM"))
+  mutate(dttm = parse_date_time(dttm, "dmy_HM")) # n.b. UTC
 
 # cohaus data has missing observations, rebuild dttm with 30 min intervals
 dttm <- seq(from = dmy_hm("01012022 00:00"), 
             to = dmy_hm("29022024 23:30"), 
             by = "30 min") |> 
-  as_datetime()
+      as_datetime()
+
+attr(dttm, "tzone")
 
 # look up function that returns NA if kWh data is missing
-kWhLookup <- function(x = dmy_hm("01012022 09:00"), source = "APT") {
+kWhLookup <- function(x, source) {
   tmp <- cohaus |> filter(dttm == x, sourceID == source) |> 
     pull(kWh)
   ifelse(is_null(tmp), NA, tmp)
@@ -77,7 +79,7 @@ getAklSunrise <- function(x = Sys.Date()) {
                           keep = c("sunrise","sunset"), 
                           tz = "Pacific/Auckland"
   )
-  return(as_datetime(tmp[,4]))
+  return(force_tz(as_datetime(tmp[,4]),tzone = "UTC"))
 }
 
 getAklSunset <- function(x = Sys.Date()) {
@@ -87,17 +89,23 @@ getAklSunset <- function(x = Sys.Date()) {
                           keep = c("sunrise","sunset"), 
                           tz = "Pacific/Auckland"
   )
-  return(as_datetime(tmp[,5]))
+  return(force_tz(as_datetime(tmp[,5]),tzone = "UTC"))
 }
 
 # dummy variable if sun is up
 # 
 isSunUp <- function(x) {
-  x <- force_tz(x, tzone = "Pacific/Auckland")
+  x <- as_datetime(x) |> 
+    force_tz(tzone = "UTC")
+  
   ifelse(as_datetime(x) > getAklSunrise(as_date(x)) &
            as_datetime(x) < getAklSunset(as_date(x)), 
-         1, 0)
+         1,0)
 }
+
+now() |> force_tz(tzone = "UTC")
+
+isSunUp(now()+hours(12))
 
 sunUp <- cohaus_wide$dttm |> 
   map_lgl(isSunUp)
@@ -105,7 +113,7 @@ sunUp <- cohaus_wide$dttm |>
 # dummy variable if work day
 #                    
 isWorkDay <- function(x) {
-  ifelse(lubridate::wday(x) %in% c(1,7), FALSE, # 1 = Sun, 7 = Sat
+  ifelse(wday(x) %in% c(1,7), FALSE, # 1 = Sun, 7 = Sat
          ifelse(as_date(x) %in% aklhols$ObsDate, FALSE, TRUE))
 }
 
@@ -115,28 +123,31 @@ workDay <- cohaus_wide$dttm |>
 # add sunUp and workday to cohaus_wide 
 cohaus_wide <- cohaus_wide |> 
   add_column(sunUp = as_factor(sunUp),
-             workDay = as_factor(workDay))
+             workDay = as_factor(workDay)) |> 
+  fill(c(sunUp,workDay), .direction = "down")
 
 # read in NIWA temps and wind data from Auckland MOTAT EWS station
 # 
 motattemp <- read_csv("data/motattemp.csv", 
                       col_types = cols(Date = col_date(format = "%Y%m%d"), 
-                                       Hour = col_time(format = "%H")))
+                                       Hour = col_time(format = "%H")))  |> 
+          fill(!c(Date,Hour), .direction = "down")
 
 motatwind <- read_csv("data/motatwind.csv", 
                       col_types = cols(Date = col_date(format = "%Y%m%d"), 
-                                       Hour = col_time(format = "%H")))
+                                       Hour = col_time(format = "%H"))) |>
+          fill(!c(Date,Hour), .direction = "down")
 
 # join motat temp and wind tibbles, compute ambient temperature
 # formula: australian bureau of meteorology
 # https://www.weather.gov/media/epz/wxcalc/vaporPressure.pdf
 # 
 weather <- motattemp |> left_join(motatwind, by = c("Date","Hour")) |> 
-  fill(!c(Date,Hour), .direction = "down") |> 
   mutate(dttm = ymd(Date) + hms(Hour),
          rho = RH/100 * 6.105 * exp(17.27 * Tdry/(Tdry + 237.7)),
          AT = Tdry + 0.33 * rho - 0.7 * Spd - 4) |> 
-  select(dttm, Tdry, RH, Spd, AT)
+  select(dttm, Tdry, RH, Spd, AT) |> 
+  fill(!dttm, .direction = "down")
 
 cohaus_wide <- cohaus_wide |> 
   left_join(weather, by = "dttm")
@@ -148,7 +159,7 @@ cohaus_wide <- cohaus_wide |>
 # create dummy variables for hour, week, month, quarter, year 
 # 
 periodDummy <- function(x = now(), y = "hour") {
-  x <- force_tz(x, tzone = "Pacific/Auckland")
+  x <- force_tz(x, tzone = "UTC")
   ifelse(y == "hour", paste0("H",sprintf("%02d", hour(x))),
          ifelse(y == "week", paste0("W",sprintf("%02d", week(x))),
                 ifelse(y == "month", paste0("M",sprintf("%02d", month(x))),
@@ -166,17 +177,25 @@ month <- map2_chr(cohaus_wide$dttm, "month", periodDummy)
 quarter <- map2_chr(cohaus_wide$dttm, "quarter", periodDummy)
 year <- map2_chr(cohaus_wide$dttm, "year", periodDummy)
 
+# add date dummy variables as variates to cohaus_wide
+#
 cohaus_wide <- cohaus_wide |>
   mutate(hour = as_factor(hour),
          week = as_factor(week),
          month = as_factor(month),
          quarter = as_factor(quarter),
          year = as_factor(year)
-         )
+         ) 
+
+# check for NAs
+# 
+na_rows <- cohaus_wide %>%
+  filter(rowSums(is.na(.)) > 0)
+glimpse(na_rows)
 
 # write to rds
+# 
 cohaus_wide |> write_rds("data/cohaus_wide.rds")
 
-glimpse(cohaus_wide)
 
 
